ba6753fe42e53ba4d2e590467f5a63fa092e3fda
Nanasuki <1447731440@qq.com>
2022-03-14 21:16:54 +0800

    修改执行子程序

--- src/pages/components/menu3.vue
+++ src/pages/components/menu3.vue
@@ -10,7 +10,7 @@
 
       <q-card-actions align="right">
         <q-btn flat @click = "runExec">Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
+        <q-btn flat @click = "runExec">Action 2</q-btn>
       </q-card-actions>
     </q-card>
 
@@ -21,8 +21,8 @@
       </q-card-section>
 
       <q-card-actions align="around">
-        <q-btn flat>Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
+        <q-btn flat @click = "runExec">Action 1</q-btn>
+        <q-btn flat @click = "runExec">Action 2</q-btn>
       </q-card-actions>
     </q-card>
 
@@ -53,42 +53,32 @@
 </template>
 <script>
 export default {
-  const exec = require('child_process').exec
- 
-  // 任何你期望执行的cmd命令，ls都可以
-  let cmdStr = './app.sh'
-  // 执行cmd命令的目录，如果使用cd xx && 上面的命令，这种将会无法正常退出子进程
-  let cmdPath = '~/ndn-app/' 
-  // 子进程名称
-  let workerProcess
+
   methods: {
-    runExec() {
+    runExec () {
+      const exec = require('child_process').exec
+      // 任何你期望执行的cmd命令，ls都可以
+      const cmdStr = './app.sh'
+      // 执行cmd命令的目录，如果使用cd xx && 上面的命令，这种将会无法正常退出子进程
+      const cmdPath = '~/ndn-app/'
       // 执行命令行，如果命令不需要路径，或就是项目根目录，则不需要cwd参数：
-      workerProcess = exec(cmdStr, {cwd: cmdPath})
+      const workerProcess = exec(cmdStr, { cwd: cmdPath })// eslint-disable-line no-unused-vars
       // 不受child_process默认的缓冲区大小的使用方法，没参数也要写上{}：workerProcess = exec(cmdStr, {})
- 
       // 打印正常的后台可执行程序输出
       workerProcess.stdout.on('data', function (data) {
-      console.log('stdout: ' + data);
-      });
- 
+        console.log('stdout: ' + data)
+      })
       // 打印错误的后台可执行程序输出
       workerProcess.stderr.on('data', function (data) {
-        console.log('stderr: ' + data);
-      });
- 
-      // 退出之后的输出
-      workerProcess.on('close', function (code) {
-        console.log('out code：' + code);
+        console.log('stderr: ' + data)
       })
     }
-  }
+  },
   data () {
     return {
       lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }
-  },
-
+  }
 }
 </script>
 <style lang="sass" scoped>
