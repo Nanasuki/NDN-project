7f12410fff63e7344919c4d577f7f0595145d25a
Nanasuki <1447731440@qq.com>
2022-03-15 13:55:45 +0800

    执行子程序第三次修改

--- src/pages/components/menu3.vue
+++ src/pages/components/menu3.vue
@@ -53,18 +53,25 @@
 </template>
 <script>
 const exec = require('child_process').exec
-
 // 任何你期望执行的cmd命令，ls都可以
-const cmdStr = './app.sh'
+const cmdStr = './build-ndnnode.sh'
 // 执行cmd命令的目录，如果使用cd xx && 上面的命令，这种将会无法正常退出子进程
-const cmdPath = '~/ndn-app/'
+const cmdPath = 'D:\\'
 
 export default {
   methods: {
     runExec () {
       console.log('执行命令行')
       // 执行命令行，如果命令不需要路径，或就是项目根目录，则不需要cwd参数：
-      exec(cmdStr, { cwd: cmdPath })
+      exec(cmdStr, { cwd: cmdPath, encoding: 'gbk' },
+        function (error, stdout, stderr) {
+          if (error) {
+            console.error('error: ' + error)
+            return
+          }
+          console.log('stdout: ' + stdout)
+          console.log('stderr: ' + typeof stderr)
+        })
       // 不受child_process默认的缓冲区大小的使用方法，没参数也要写上{}：workerProcess = exec(cmdStr, {})
     }
   },
